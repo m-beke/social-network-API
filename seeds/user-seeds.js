dc5f09b89c1538364bef651c4ff13c6392dc1db2
Matthew Beke <mattbeke@gmail.com>
2024-03-03 20:27:29 -0500

    added starter code

---
+++ seeds/user-seeds.js
@@ -0,0 +1,20 @@
+const seedUsers = [
+  {
+    username: "Yennefer",
+    email: "yennefer@gmail.com",
+  },
+  {
+    username: "Triss",
+    email: "triss@gmail.com",
+  },
+  {
+    username: "Ciri",
+    email: "ciri@gmail.com",
+  },
+  {
+    username: "Geralt",
+    email: "geralt@gmail.com",
+  },
+];
+
+module.exports = seedUsers;
